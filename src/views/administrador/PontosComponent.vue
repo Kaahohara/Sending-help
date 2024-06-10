b0f80d1c65eda158784f4df2ee01ca4a3184a00b
Karina <liraoharak@gmail.com>
2024-06-10 18:31:05 -0300

    Fix

--- src/views/administrador/PontosComponent.vue
+++ src/views/administrador/PontosComponent.vue
@@ -12,13 +12,20 @@
               <input type="tel" v-model="pontoData.telefone" id="telefone" name="telefone" placeholder="Telefone:" required>
             </div>
             <div class="input-group-ong">
-              <select v-model="pontoData.endereco.cidade" id="cidade" name="cidade" required>
-                <option disabled value="">Selecione uma cidade</option>
-                <option value="Amazonas">Amazonas</option>
-                <option value="Mato Grosso">Mato Grosso</option>
-                <option value="Rio de Janeiro">Rio de Janeiro</option>
-              </select>
-            </div>
+          <select v-model="pontoData.endereco.estado" id="estado" name="estado" required>
+            <option disabled value="">Selecione um Estado</option>
+            <option value="Amazonas">Amazonas</option>
+            <option value="Mato Grosso">Mato Grosso</option>
+            <option value="Rio de Janeiro">Rio de Janeiro</option>
+          </select>
+        </div>  <div class="input-group-ong">
+          <select v-model="pontoData.endereco.cidade" id="cidade" name="cidade" required>
+            <option disabled value="">Selecione uma cidade</option>
+            <option value="Lagos">Lagos</option>
+            <option value="Americana">Americana</option>
+            <option value="Frederico">Frederico Westphalen</option>
+          </select>
+        </div>
             <div class="input-group-ong">
               <input type="text" v-model="pontoData.endereco.rua" id="rua" name="rua" placeholder="Rua:" required>
             </div>
@@ -98,6 +105,7 @@ export default {
     };
   },
   computed: {
+ 
     filteredPontos() {
       return this.pontos;
     }
@@ -106,12 +114,21 @@ export default {
     this.fetchPontos();
   },
   methods: {
+
     verificalogado() {
       return !!localStorage.getItem('token');
     },
     registerPonto() {
       if (this.pontoData.telefone.length > 11) {
         this.cadastroMessage = 'O telefone deve ter no máximo 11 dígitos.';
+        return;
+      } if (this.pontoData.endereco.numero <= 0) {
+    this.cadastroMessage = 'Numero Inválido.';
+    return;
+  }
+
+  if (!this.isValidCEP(this.pontoData.endereco.cep)) {
+        this.cadastroMessage = 'CEP inválido.';
         return;
       }
 
@@ -126,10 +143,10 @@ export default {
       .then(response => {
         response.data;
         this.cadastroMessage = 'Ponto registrado com sucesso';
-        this.fetchPontos(); // Atualiza a lista de pontos após o registro
+        this.fetchPontos();
       })
       .catch(error => {
-        this.cadastroMessage = 'Falha ao registrar o ponto';
+        this.cadastroMessage = 'Falha ao registrar o ponto/Telefone já registrado';
       });
     },
     fetchPontos() {
@@ -146,6 +163,10 @@ export default {
         console.error('Erro ao buscar ponto:', error);
       });
     },
+    isValidCEP(cep) {
+      const cepRegex = /^[0-9]{5}-?[0-9]{3}$/;
+      return cepRegex.test(cep);
+    },
     getOngImage(ponto) {
       return '@/assets/Amazonia.png';
     },
@@ -155,5 +176,3 @@ export default {
   }
 }
 </script>
-
-
--- src/views/doador/DoacaoComponent.vue
+++ src/views/doador/DoacaoComponent.vue
@@ -33,55 +33,71 @@
     </div> 
 
     
-  <div v-if="!verificalogado()">
-  <div class="form-box">
-    <div class="box-form">
-      <div class="lados back-green" style="display: flex; align-items: center; justify-content: center;">
-        <img src="@/assets/logo.png" width="200px" alt="logo">
-      </div>
-      <div class="lados back-white">
-          <div v-if="!hasLogin" class="cadastro-container">
-            {{cadastroMessage}}
-            <p class="subtitulo">CADASTRO</p>
-            <form @submit.prevent="registerUser">
-              <div class="input-group">
-                <input type="text" v-model="doadorData.name" placeholder="Nome" required>
-              </div>
-              <div class="input-group">
-                <input type="email" v-model="registerData.login" placeholder="Username" required>
-              </div>
-              <div class="input-group">
-                <input type="password" v-model="registerData.password" placeholder="Password" required>
-              </div>
-              <div class="input-group">
-                <input type="number" v-model="doadorData.phone" placeholder="Phone" required>
-              </div>
-          
-              <button type="submit">Registrar</button>
-            </form>
-            <a @click="toggleForm">Já tem uma conta? Faça login</a>
+    <div v-if="!verificalogado()">
+      <div class="form-box">
+        <div class="box-form">
+          <div class="lados back-green" style="display: flex; align-items: center; justify-content: center;">
+            <img src="@/assets/logo.png" width="200px" alt="logo">
           </div>
-          <div v-else class="login-container">
-           <div v-if="!loginMessage">Tente Novamente!</div>
-            <p class="subtitulo">LOGIN</p>
-            <form @submit.prevent="submitForm">
-              <div class="input-group">
-                <input type="email" v-model="loginData.login" placeholder="Username" required>
-              </div>
-              <div class="input-group">
-                <input type="password" v-model="loginData.password" placeholder="Password" required>
-              </div>
-              <button type="submit">Login</button>
-            </form>
-          
-            <a @click="toggleForm">Ainda não tem uma conta? Cadastre-se</a>
+          <div class="lados back-white">
+            <div v-if="!hasLogin" class="cadastro-container">
+              {{cadastroMessage}}
+              <div  v-if="!part2">
+              <p class="subtitulo">CADASTRO</p>
+              <form @submit.prevent="registerUser">
+             
+                <div class="input-group">
+                  <input type="email" v-model="loginData.login" placeholder="Username" required>
+                </div>
+                <div class="input-group">
+                  <input type="password" v-model="loginData.password" placeholder="Password" required>
+                </div>
+               
+
+                <button type="submit">Registrar</button>
+              </form>
+              <a @click="toggleForm">Já tem uma conta? Faça login</a>
+            </div>
+              <div v-if="part2">
+              <p class="subtitulo">Complete seu Cadastro</p>
+              <form @submit.prevent="registerDoador">
+                <div class="input-group">
+                  <input type="text" v-model="doadorData.nomeCompleto" placeholder="nomeCompleto" required>
+                </div>
+                
+                <div class="input-group">
+                  <input type="date" v-model="doadorData.dataNascimento" placeholder="Data de Nascimento" required>
+                </div>
+                <div class="input-group">
+                  <input type="number" v-model="doadorData.telefoneContato" placeholder="telefone" required>
+                </div>
+                <div class="input-group">
+                  <input type="text" v-model="doadorData.cpf" placeholder="CPF" required>
+                </div>
+
+                <button type="submit">Registrar</button>
+              </form>
+            </div>
+          </div>
+            <div v-else class="login-container">
+              <div v-if="!loginMessage">Tente Novamente!</div>
+              <p class="subtitulo">LOGIN</p>
+              <form @submit.prevent="submitForm">
+                <div class="input-group">
+                  <input type="email" v-model="loginData.login" placeholder="Username" required>
+                </div>
+                <div class="input-group">
+                  <input type="password" v-model="loginData.password" placeholder="Password" required>
+                </div>
+                <button type="submit">Login</button>
+              </form>
+
+              <a @click="toggleForm">Ainda não tem uma conta? Cadastre-se</a>
+            </div>
           </div>
         </div>
       </div>
     </div>
-
-  </div>
-
     <div v-if="verificalogado()">
 
     <div class="form-doacao">
@@ -155,21 +171,8 @@ export default {
   data() {
   
       return {
-      hasLogin: true,
-      loginData: {
-        login: '',
-        password: ''
-      },
-      registerData: {
-        login: '',
-        password: '',
-        role: 'ADMIN'
-      },
-      doadorData:{
-        name:'',
-        phone:''
-      },
-  
+
+     
       donationData: {
         tipo: '',
         cpf:'',
@@ -186,9 +189,29 @@ export default {
      
         }
       },
+      hasLogin: true,
+      part2: false,
+      loginData: {
+        login: '',
+        password: ''
+      },
+      registerData: {
+        login: '',
+        password: '',
+        role: 'ADMIN'
+      },
+      loginRegisterData:{
+        login: '',
+        password: ''
+      },
+      doadorData: {
+        nomeCompleto: '',
+        telefoneContato: '',
+        cpf: '',
+        dataNascimento:''
+      },
       loginMessage: true,
       cadastroMessage: null
-    
     };
   },
   
@@ -199,7 +222,9 @@ export default {
       return !!localStorage.getItem('token');
     },
 
-   
+    toggleForm() {
+      this.hasLogin = !this.hasLogin;
+    },
     
     registerDonation() {
       if(this.donationData.endereco.cidade=='Amazonas'){
@@ -241,92 +266,146 @@ export default {
           });
         }},
         
-        submitForm() {
-      apiClient.post('/auth/login', this.loginData, {
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': '*/*'
-        }
-      })
-      .then(response => {
-        const authToken = response.data.token;
-        localStorage.setItem('token', authToken);
        
-        apiClient.get('/doadores', {
-          headers: {
-            'Content-Type': 'application/json',
-            'Accept': '*/*',
-            'Authorization': `Bearer ${authToken}`
-          }
-        })
-        .then(response => {
+    submitForm() {
+
+          apiClient.post('/auth/login', this.loginData, {
+            headers: {
+              'Content-Type': 'application/json',
+              'Accept': '*/*'
+            }
+          })
         
-          this.loginMessage = null;
-          const doadores = response.data.filter(doador => doador.email === this.loginData.login);
-          if (doadores.length > 0) { 
-            const primeiroDoador = doadores[0]; 
-            localStorage.setItem('nome', primeiroDoador.name);
-            localStorage.setItem('cpf', primeiroDoador.cpf);
-             window.location.reload();
-          } else {
-            this.loginMessage = false;
-          }
-        })
-        .catch(error => {
-          this.loginMessage = false;
-        });
-      })
-      .catch(error => {
-        this.loginMessage = false;
-      });
-    }},
-    
-    registerUser() {
-      apiClient.post('/auth/register', this.registerData, {
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': '*/*'
-        }
-      })
-      .then(response => {
+              .then(response => {
+              this.cadastroMessage = true;
+              const authToken = response.data.token;
+            
+           
+              
+                apiClient.get('/doador', {
+                  headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': '*/*',
+                    'Authorization': `Bearer ${authToken}`
+                  }
+                })
+                  .then(response => {
+                      this.loginMessage = null;
+                      
+                    const doadores=response.data.filter(doador => doador.email === this.loginData.login);
+                    if (doadores.length === 0 || doadores[0] == null || doadores[0].length === 0) {
+                        this.part2 = true;
+                        localStorage.setItem('login',this.loginData.login );
+                        this.loginMessage = true;
+                        this.hasLogin=false;
+                      }else{
+                        localStorage.setItem('nome', doadores[0].nomeCompleto);
+                        localStorage.setItem('cpf',doadores[0].cpf);
+                        localStorage.setItem('token', authToken);
+                     
+                      this.$router.push('/doador-doar');
+                    }
+                    })
        
-        this.cadastroMessage = true;
-        const authToken = response.data.token;
-        const name = response.data.name;
-        localStorage.setItem('token', authToken);
-    
-        this.doadorData.email = this.registerData.login;
-        apiClient.post('/doadores', this.doadorData, {
-          headers: {
-            'Content-Type': 'application/json',
-            'Accept': '*/*',
-            'Authorization': `Bearer ${authToken}`
-          }
-        })
-        .then(response => {
-         
-          localStorage.setItem('nome', this.doadorData.name);
-          localStorage.setItem('cpf', primeiroDoador.cpf);
-          window.location.reload();
-
-        })
-        .catch(error => {
-          
-          this.cadastroMessage = 'Tente Novamente';
-        });
-      })
-      .catch(error => {
-        if (error.response && error.response.status === 400) {
-          this.cadastroMessage = 'E-mail ja cadastrado'; 
-          }else{
-        this.cadastroMessage = 'Tente Novamente';
+    })
+    .catch(error => {
+               
+                      this.loginMessage = false;
+    })
+  },
+    registerDoador() {
+      
+      this.cadastroMessage = null;
+      if (!this.validarCPF(this.doadorData.cpf)) {
+        this.cadastroMessage = 'CPF inválido';
+        return;
       }
-    });
+      apiClient.post('/auth/login', this.loginData, {
+                  headers: {
+                  'Content-Type': 'application/json',
+                  'Accept': '*/*'
+                }
+                })
+                
+                  .then(response => {
+                    const authToken = response.data.token;
+                    
+                  
+                    this.doadorData.email = this.loginData.login
+                    if (this.doadorData.nomeCompleto.length <= 11) {
+                        this.cadastroMessage='Insira o nome completo';
+                       
+                    }
+                    if (this.doadorData.nomeCompleto.length > 11) {
+ 
+                    apiClient.post('/doador', this.doadorData, {
+                      headers: {
+                        'Content-Type': 'application/json',
+                        'Accept': '*/*',
+                        'Authorization': `Bearer ${authToken}`
+                      }
+                    })
+                  
+                          .then(response => {
+                            if(response.status===201){
+                              localStorage.setItem('login', this.doadorData.login);
+                              localStorage.setItem('nome', this.doadorData.nomeCompleto);
+                              localStorage.setItem('cpf', this.doadorData.cpf);
+                              localStorage.setItem('token', authToken);
+                              this.$router.push('/doador-doar');
+                            }else{
+                              this.cadastroMessage = 'CPF já cadastrado';
+                            }
+                            })
+                          .catch(error => {
+                              if (error.response) {
+                                  this.cadastroMessage = 'CPF já cadastrado';
+                              } else {
+                                  this.cadastroMessage = 'Tente Novamente';
+                              }
+                                })
+                 
+            
+          } 
+         
+        })
+        
+            .catch(error => {
+              if (error.response) {
+                                  this.cadastroMessage = 'CPF já cadastrado';
+                              } else {
+                                  this.cadastroMessage = 'Tente Novamente';
+                              }
+                  
+                  })
     },
+    registerUser() {
+      
+          apiClient.post('/auth/register', this.loginData, {
+            headers: {
+              'Content-Type': 'application/json',
+              'Accept': '*/*'
+            }
+          })
+          .then(response => {
+                 this.part2=true;
+                 
+      this.cadastroMessage = null;
+                    
+      })
+               .catch(error => {
+                  if (error.response ) {
+                    this.cadastroMessage = 'E-mail já cadastrado';
+                  } else {
+                    this.cadastroMessage = 'Tente Novamente';
+                  }
+                })
+                
+    },
+  
 
-    toggleForm() {
-      this.hasLogin = !this.hasLogin;
-    }
+  }
+   
   }
 
 </script>
