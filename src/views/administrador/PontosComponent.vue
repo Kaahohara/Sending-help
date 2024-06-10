9190ad1a243f0b9d2b7653b7a8dc8c16b118ac82
Karina <liraoharak@gmail.com>
2024-06-10 17:21:42 -0300

    FixVerificaTelefone

--- src/views/administrador/PontosComponent.vue
+++ src/views/administrador/PontosComponent.vue
@@ -1,34 +1,24 @@
 <template>
   <div>
-
-
-
-    
-  <div  class="form-box">
-       
-           
-            
-            <form @submit.prevent="registerPonto">
-              <p style="color: red; font-size: large; text-align: center;">{{cadastroMessage}}</p>
+    <div class="form-box">
+      <form @submit.prevent="registerPonto">
+        <p style="color: red; font-size: large; text-align: center;">{{ cadastroMessage }}</p>
         <div class="ong-container">
-         <div class="lados">
+          <div class="lados">
             <div class="input-group-ong">
               <input type="text" v-model="pontoData.nome" id="nome" name="nome" placeholder="Nome do ponto de coleta:" required>
             </div>
             <div class="input-group-ong">
               <input type="tel" v-model="pontoData.telefone" id="telefone" name="telefone" placeholder="Telefone:" required>
             </div>
-        
-         
             <div class="input-group-ong">
-          <select v-model="pontoData.endereco.cidade" id="cidade" name="cidade" required>
-            <option disabled value="">Selecione uma cidade</option>
-            <option value="Amazonas">Amazonas</option>
-            <option value="Mato Grosso">Mato Grosso</option>
-            <option value="Rio de Janeiro">Rio de Janeiro</option>
-          </select>
-        </div>
-
+              <select v-model="pontoData.endereco.cidade" id="cidade" name="cidade" required>
+                <option disabled value="">Selecione uma cidade</option>
+                <option value="Amazonas">Amazonas</option>
+                <option value="Mato Grosso">Mato Grosso</option>
+                <option value="Rio de Janeiro">Rio de Janeiro</option>
+              </select>
+            </div>
             <div class="input-group-ong">
               <input type="text" v-model="pontoData.endereco.rua" id="rua" name="rua" placeholder="Rua:" required>
             </div>
@@ -36,24 +26,20 @@
               <input type="number" v-model="pontoData.endereco.numero" id="numero" name="numero" placeholder="Número:" required>
             </div>
             <div class="input-group-ong">
-              <input type="text"  v-model="pontoData.endereco.bairro" id="bairro" name="bairro" placeholder="Bairro:" required>
+              <input type="text" v-model="pontoData.endereco.bairro" id="bairro" name="bairro" placeholder="Bairro:" required>
             </div>
             <div class="input-group-ong">
               <input type="text" v-model="pontoData.endereco.cep" id="cep" name="cep" placeholder="CEP:" required>
             </div>
-          
             <button type="submit">Registrar</button>
-          <br>
-            <a  style="color: aliceblue;" @click="toggleForm">Já tem uma conta? Faça login</a>
+            <br>
+            <a style="color: aliceblue;" @click="toggleForm">Já tem uma conta? Faça login</a>
           </div>
         </div>
       </form>
     </div>
-
-   
-    
     <div class="containerong">
-        <div v-for="ponto in filteredPontos" :key="ponto.id" class="containerong-item">
+      <div v-for="ponto in filteredPontos" :key="ponto.id" class="containerong-item">
         <div class="picture">
           <img src='@/assets/Amazonia.png' alt="ongimage">
         </div>
@@ -63,33 +49,30 @@
         </div>
       </div>
     </div>
-  
-  <div class="cont">
-    <div class="lados">
-      <img src="@/assets/logo.png" width="200px" alt="logo">
-    </div>
-    <div class="lados">
-      <p>
-        Site de doações para desastres naturais. Em tempos de crise, cada gesto de solidariedade faz uma enorme diferença. Junte-se a nós para apoiar comunidades afetadas por desastres ao redor do mundo.
-      </p>
-    </div>
-    <div class="lados">
-      <p>E-mail:Centro@gmail.com</p>
-      <p>Telefone:55 98783-46655</p>
-      <div class="icons">
-        <div class="lados">
-          <img src="@/assets/whats.png" alt="whatsapp">
-        </div>
-        <div class="lados">
-          <img src="@/assets/instagram.png" alt="instagram">
-        </div>
-        <div class="lados">
-          <img src="@/assets/facebook.png " alt="facebook">
+    <div class="cont">
+      <div class="lados">
+        <img src="@/assets/logo.png" width="200px" alt="logo">
+      </div>
+      <div class="lados">
+        <p>Site de doações para desastres naturais. Em tempos de crise, cada gesto de solidariedade faz uma enorme diferença. Junte-se a nós para apoiar comunidades afetadas por desastres ao redor do mundo.</p>
+      </div>
+      <div class="lados">
+        <p>E-mail:Centro@gmail.com</p>
+        <p>Telefone:55 98783-46655</p>
+        <div class="icons">
+          <div class="lados">
+            <img src="@/assets/whats.png" alt="whatsapp">
+          </div>
+          <div class="lados">
+            <img src="@/assets/instagram.png" alt="instagram">
+          </div>
+          <div class="lados">
+            <img src="@/assets/facebook.png" alt="facebook">
+          </div>
         </div>
       </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
@@ -98,21 +81,20 @@ import apiClient from '@/axios';
 export default {
   data() {
     return {
-  
       pontoData: {
         nome: '',
         telefone: '',
-        endereco:{
-        cep: '',
-        estado: '',
-        cidade: '',
-        rua: '',
-        bairro: '',
-        numero: ''
+        endereco: {
+          cep: '',
+          estado: '',
+          cidade: '',
+          rua: '',
+          bairro: '',
+          numero: ''
         }
       },
-    
-      pontos: []
+      pontos: [],
+      cadastroMessage: ''
     };
   },
   computed: {
@@ -124,33 +106,33 @@ export default {
     this.fetchPontos();
   },
   methods: {
-    verificalogado(){
+    verificalogado() {
       return !!localStorage.getItem('token');
     },
-
- 
     registerPonto() {
+      if (this.pontoData.telefone.length > 11) {
+        this.cadastroMessage = 'O telefone deve ter no máximo 11 dígitos.';
+        return;
+      }
 
-          const authToken = localStorage.getItem('token');
-          
-          apiClient.post('/ponto', this.pontoData, {
-            headers: {
-              'Content-Type': 'application/json',
-              'Accept': '*/*',
-              'Authorization': `Bearer ${authToken}`
-            }
-          })
-          .then(response => {
-            response.data;
-            
-          })
-          .catch(error => {
-            this.cadastroMessage = 'Falha ao registrar o ponto';
-          });
-        },
-        
-   
-fetchPontos() {
+      const authToken = localStorage.getItem('token');
+      apiClient.post('/ponto', this.pontoData, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': '*/*',
+          'Authorization': `Bearer ${authToken}`
+        }
+      })
+      .then(response => {
+        response.data;
+        this.cadastroMessage = 'Ponto registrado com sucesso';
+        this.fetchPontos(); // Atualiza a lista de pontos após o registro
+      })
+      .catch(error => {
+        this.cadastroMessage = 'Falha ao registrar o ponto';
+      });
+    },
+    fetchPontos() {
       apiClient.get('/ponto', {
         headers: {
           'Content-Type': 'application/json',
@@ -165,8 +147,7 @@ fetchPontos() {
       });
     },
     getOngImage(ponto) {
-    
-      return  '@/assets/Amazonia.png';
+      return '@/assets/Amazonia.png';
     },
     toggleForm() {
       this.hasLogin = !this.hasLogin;
@@ -174,3 +155,5 @@ fetchPontos() {
   }
 }
 </script>
+
+
